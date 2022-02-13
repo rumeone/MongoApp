3c719254b5446f2fd11b9839e932cfdeed17ba9e
rumeone <yuaroslav_29@mail.ru>
2022-02-13 22:33:18 +0700

    Add active class for navbar

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,18 @@
-const {Router} = require('express');
+const {Router, response} = require('express');
 const router = Router();
 
 router.get('/', (req,res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Todos List',
+        isIndex: true
+    });
+})
+
+router.get('/create', (req,res) => {
+    res.render('create', {
+        title: 'Create Todo',
+        isCreate: true
+    })
 })
 
 module.exports = router;
