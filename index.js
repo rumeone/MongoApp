eca963f175aaa82fde83b68e95d4361f9b13909e
rumeone <yuaroslav_29@mail.ru>
2022-02-13 21:28:00 +0700

    Inclune hbs file

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const expressHandlebars = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = expressHandlebars.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 async function start() {
     try {
-    await mongoose.connect('', {
-        useNewUrlParser: true,
-        useFindAndModify: false
-    });
-    app.listen(PORT, () => {
-        console.log(`Server has been listening ${PORT}.`);
-    });
+        await mongoose.connect('', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+        app.listen(PORT, () => {
+            console.log(`Server has been listening ${PORT}.`);
+        });
     } catch (error) {
         console.log(error);
     }
-}
+};
 
+
+
+start();
