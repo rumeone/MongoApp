5b2d38787195c4a7ce669a0142d5361b02795c0d
rumeone <yuaroslav_29@mail.ru>
2022-02-13 21:52:25 +0700

    Add new directory routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
+const todoRoutes = require('./routes/todos')
 
 const app = express();
 const hbs = expressHandlebars.create({
@@ -13,19 +14,19 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(todoRoutes);
+
 async function start() {
     try {
-        await mongoose.connect('', {
-            useNewUrlParser: true,
-            useFindAndModify: false
-        });
+        await mongoose.connect(
+            'mongodb+srv://root:root@cluster0.myinv.mongodb.net/todos');
         app.listen(PORT, () => {
-            console.log(`Server has been listening ${PORT}.`);
-        });
-    } catch (error) {
-        console.log(error);
+            console.log('Server has been started...')
+        })
+    } catch (e) {
+        console.log(e)
     }
-};
+}
 
 
 
