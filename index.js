7fb5b3acbbc4e73f6304a42cdcb747f212ebd498
rumeone <yuaroslav_29@mail.ru>
2022-02-14 01:18:11 +0700

    the end of the project

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const expressHandlebars = require('express-handlebars');
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
 
-const app = express();
-const hbs = expressHandlebars.create({
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
-});
+})
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.use(todoRoutes);
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
 
 async function start() {
     try {
         await mongoose.connect(
-            'mongodb+srv://root:root@cluster0.myinv.mongodb.net/todos');
+            'mongodb+srv://root:root@cluster0.myinv.mongodb.net/todos',
+        )
         app.listen(PORT, () => {
             console.log('Server has been started...')
         })
@@ -28,6 +34,6 @@ async function start() {
     }
 }
 
+start()
 
-
-start();
+//'mongodb+srv://root:root@cluster0.myinv.mongodb.net/todos'
--- routes/todos.js
+++ routes/todos.js
@@ -1,18 +1,39 @@
-const {Router, response} = require('express');
-const router = Router();
+const { Router } = require('express')
+const Todo = require('../models/Todo')
+const router = Router()
 
-router.get('/', (req,res) => {
+router.get('/', async (req, res) => {
+    const todos = await Todo.find({}).lean()
+
     res.render('index', {
-        title: 'Todos List',
-        isIndex: true
-    });
+        title: 'Todos list',
+        isIndex: true,
+        todos
+    })
 })
 
-router.get('/create', (req,res) => {
+router.get('/create', (req, res) => {
     res.render('create', {
-        title: 'Create Todo',
+        title: 'Create todo',
         isCreate: true
     })
+})
+
+router.post('/create', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    })
+    todo.save();
+    res.redirect('/')
+    console.log(req.body);
+})
+
+router.post('/complete', async (req,res) => {
+    const todo = await Todo.findById(req.body.id);
+
+    todo.completed = !!req.body.completed;
+    await todo.save();
+    res.redirect("/")
 })
 
 module.exports = router;
